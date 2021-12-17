901af20717976f87a1a11b2a12e21ebc16234742
王丛 <wangcong@macbook.m1>
2021-12-17 11:22:48 +0800

    all

--- src/Resources/views/admin/form/uEditor.blade.php
+++ src/Resources/views/admin/form/uEditor.blade.php
@@ -3,40 +3,65 @@
     <div class="col-sm-8">
         @include('admin::form.error')
         {{-- 这个style可以限制他的高度，不会随着内容变长 --}}
-        <textarea type='text/plain' style="height:200px;" id='ueditor{{$id}}'  name="{{$name}}" placeholder="{{ $placeholder }}" {!! $attributes !!}  class='ueditor'>{!! old($column, $value) !!}</textarea>
+        <textarea type='text/plain' style="height:200px;" id='ueditor{{$id}}' name="{{$name}}"
+                  placeholder="{{ $placeholder }}"
+                  {!! $attributes !!}  class='ueditor'>{!! old($column, $value) !!}</textarea>
         <script>
-            var ueditor{{$id}};
-           // $(function () {
-                //解决第二次进入加载不出来的问题
-/*                UE.delEditor("ueditor{{$id}}");
-                // 默认id是ueditor
-                var ueditor{{$id}} = UE.getEditor('ueditor{{$id}}', {
-                    serverUrl: "{{config('wangpkg.ueditor_api')}}",
-                    // 自定义工具栏
-                    toolbars: [
-                        ['bold', 'italic', 'underline', 'strikethrough', 'blockquote', 'insertunorderedlist', 'insertorderedlist', 'justifyleft', 'justifycenter', 'justifyright', 'link','unlink', 'insertimage', 'source',
-                           'insertvideo', '|','removeformat', 'formatmatch',
-                            'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|','preview',
-                            'fullscreen']
-                    ],
-                    elementPathEnabled: false,
-                    enableContextMenu: false,
-                    autoClearEmptyNode: true,
-                    wordCount: false,
-                    imagePopup: false,
-                    autoHeightEnabled:false,
-                    autotypeset: {indent: true, imageBlockLine: 'center'}
-                });
-                });*/
+            $(function () {
+                        //解决第二次进入加载不出来的问题
+                        //UE.delEditor("ueditor{{$id}}");
+                        // 默认id是ueditor
+                        var ueditor{{$id}} = UE.getEditor('ueditor{{$id}}', {
+                            serverUrl: "{{config('wangpkg.ueditor_api')}}",
+                            // 自定义工具栏
+                            toolbars: [
+                                ['bold', 'italic', 'underline', 'strikethrough', 'blockquote', 'insertunorderedlist', 'insertorderedlist', 'justifyleft', 'justifycenter', 'justifyright', 'link', 'unlink', 'insertimage', 'source',
+                                    'insertvideo', '|', 'removeformat', 'formatmatch',
+                                    'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|', 'preview',
+                                    'fullscreen']
+                            ],
+                            elementPathEnabled: false,
+                            enableContextMenu: false,
+                            autoClearEmptyNode: true,
+                            wordCount: false,
+                            imagePopup: false,
+                            autoHeightEnabled: false,
+                            autotypeset: {indent: true, imageBlockLine: 'center'}
+                        });
+            });
 
-            var t{{$id}};
+            /*            var t{{$id}};
             window.addEventListener('pageshow', function (event) {
-                t{{$id}} = setInterval(function(){
+            t{{$id}} = setInterval(function () {
 
-                    if(typeof(UE)!='undefined'){
-                        clearInterval(t{{$id}});
+                if (typeof (UE) != 'undefined') {
+                    clearInterval(t{{$id}});
 
-                        if (event.persisted) { // ios 有效, android 和 pc 每次都是 false
+                    if (event.persisted) { // ios 有效, android 和 pc 每次都是 false
+                        console.log(1234);
+                        //解决第二次进入加载不出来的问题
+                        //UE.delEditor("ueditor{{$id}}");
+                        // 默认id是ueditor
+                        var ueditor{{$id}} = UE.getEditor('ueditor{{$id}}', {
+                            serverUrl: "{{config('wangpkg.ueditor_api')}}",
+                            // 自定义工具栏
+                            toolbars: [
+                                ['bold', 'italic', 'underline', 'strikethrough', 'blockquote', 'insertunorderedlist', 'insertorderedlist', 'justifyleft', 'justifycenter', 'justifyright', 'link', 'unlink', 'insertimage', 'source',
+                                    'insertvideo', '|', 'removeformat', 'formatmatch',
+                                    'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|', 'preview',
+                                    'fullscreen']
+                            ],
+                            elementPathEnabled: false,
+                            enableContextMenu: false,
+                            autoClearEmptyNode: true,
+                            wordCount: false,
+                            imagePopup: false,
+                            autoHeightEnabled: false,
+                            autotypeset: {indent: true, imageBlockLine: 'center'}
+                        });
+                    } else {
+
+                        if (sessionStorage.getItem('refresh') === 'true') {
                             //解决第二次进入加载不出来的问题
                             UE.delEditor("ueditor{{$id}}");
                             // 默认id是ueditor
@@ -44,9 +69,9 @@
                                 serverUrl: "{{config('wangpkg.ueditor_api')}}",
                                 // 自定义工具栏
                                 toolbars: [
-                                    ['bold', 'italic', 'underline', 'strikethrough', 'blockquote', 'insertunorderedlist', 'insertorderedlist', 'justifyleft', 'justifycenter', 'justifyright', 'link','unlink', 'insertimage', 'source',
-                                        'insertvideo', '|','removeformat', 'formatmatch',
-                                        'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|','preview',
+                                    ['bold', 'italic', 'underline', 'strikethrough', 'blockquote', 'insertunorderedlist', 'insertorderedlist', 'justifyleft', 'justifycenter', 'justifyright', 'link', 'unlink', 'insertimage', 'source',
+                                        'insertvideo', '|', 'removeformat', 'formatmatch',
+                                        'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|', 'preview',
                                         'fullscreen']
                                 ],
                                 elementPathEnabled: false,
@@ -54,38 +79,16 @@
                                 autoClearEmptyNode: true,
                                 wordCount: false,
                                 imagePopup: false,
-                                autoHeightEnabled:false,
+                                autoHeightEnabled: false,
                                 autotypeset: {indent: true, imageBlockLine: 'center'}
                             });
-                        } else { // ios 除外
-                            if (sessionStorage.getItem('refresh') === 'true') {
-                                //解决第二次进入加载不出来的问题
-                                UE.delEditor("ueditor{{$id}}");
-                                // 默认id是ueditor
-                                var ueditor{{$id}} = UE.getEditor('ueditor{{$id}}', {
-                                    serverUrl: "{{config('wangpkg.ueditor_api')}}",
-                                    // 自定义工具栏
-                                    toolbars: [
-                                        ['bold', 'italic', 'underline', 'strikethrough', 'blockquote', 'insertunorderedlist', 'insertorderedlist', 'justifyleft', 'justifycenter', 'justifyright', 'link','unlink', 'insertimage', 'source',
-                                            'insertvideo', '|','removeformat', 'formatmatch',
-                                            'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|','preview',
-                                            'fullscreen']
-                                    ],
-                                    elementPathEnabled: false,
-                                    enableContextMenu: false,
-                                    autoClearEmptyNode: true,
-                                    wordCount: false,
-                                    imagePopup: false,
-                                    autoHeightEnabled:false,
-                                    autotypeset: {indent: true, imageBlockLine: 'center'}
-                                });
-                            }
                         }
-                        sessionStorage.removeItem('refresh');
                     }
+                    sessionStorage.removeItem('refresh');
+                }
                 },100);
-            });
-            </script>
+            });*/
+        </script>
         @include('admin::form.help-block')
     </div>
 </div>
