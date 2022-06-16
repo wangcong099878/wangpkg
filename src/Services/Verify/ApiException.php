bc2d518a976d39f4d9187aaf7ba78d9005c3d528
王丛 <wangcong@macbook.m1>
2022-06-16 19:51:39 +0800

    all

--- src/Services/Verify/ApiException.php
+++ src/Services/Verify/ApiException.php
@@ -7,6 +7,7 @@ class ApiException extends \Exception
 {
     function __construct($data = null, $errorCode = 0, $message = "", $options = [])
     {
+
         $this->errorMsg = $message;
         $this->errorCode = $errorCode;
         parent::__construct($message);
